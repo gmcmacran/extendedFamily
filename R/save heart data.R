4d917464ab12d706752fc260a3593e103c0f429b
Greg McMahan <gmcmacran@gmail.com>
2020-05-02 20:31:52 -0500

    Adding heart data importing code

---
+++ R/save heart data.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+# heart <- haven::read_dta("S:/R/data/generalized linear models and extensions/heart01.dta")
+# heart <- heart %>%
+#   tidyr::drop_na() %>%
+#   dplyr::select(-center)
+#
+# usethis::use_data(heart, overwrite = TRUE, version = 3)
+#
+# tools::checkRdaFiles("data/heart.rda")
+#
+# usethis::use_data(heart, overwrite = TRUE, version = 3, compress = "bzip2")
