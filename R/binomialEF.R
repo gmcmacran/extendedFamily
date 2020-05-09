783fcf928ae8710cebc3120167fcf0e30eb9a843
Greg McMahan <gmcmacran@gmail.com>
2020-05-08 23:23:33 -0500

    Checking links in documentation

--- R/binomialEF.R
+++ R/binomialEF.R
@@ -14,21 +14,26 @@
 #'   \item linkinv: function: the inverse of the link function.
 #'   \item variance: function: the variance as a function of the mean.
 #'   \item dev.resids function giving the deviance for each observation as a function of (y, mu, wt),
-#'         used by the residuals method when computing deviance residuals.
+#'         used by the \href{https://stat.ethz.ch/R-manual/R-devel/library/stats/html/glm.summaries.html}{residuals}
+#'         method when computing deviance residuals.
 #'   \item aic: function giving the AIC value if appropriate (but NA for the quasi- families).
 #'         More precisely, this function returns -2 ll + 2 s, where ll is the log-likelihood and s
 #'         is the number of estimated scale parameters. Note that the penalty term for the location
-#'         parameters (typically the “regression coefficients”) is added elsewhere, e.g., in glm.fit(),
-#'         or AIC(), see the AIC example in glm. See logLik for the assumptions made about the dispersion
-#'         parameter.
+#'         parameters (typically the “regression coefficients”) is added elsewhere, e.g., in
+#'         \href{https://stat.ethz.ch/R-manual/R-devel/library/stats/html/glm.html}{glm.fit}()
+#'         or \href{https://stat.ethz.ch/R-manual/R-devel/library/stats/html/AIC.html}{AIC}(),
+#'         see the AIC example in \href{https://stat.ethz.ch/R-manual/R-devel/library/stats/html/glm.html}{glm}.
+#'         See \href{https://stat.ethz.ch/R-manual/R-devel/library/stats/html/logLik.html}{logLik}
+#'         for the assumptions made about the dispersion parameter.
 #'   \item initialize: expression. This needs to set up whatever data objects are needed for the family
 #'         as well as n (needed for AIC in the binomial family) and mustart (See
 #'         \href{https://stat.ethz.ch/R-manual/R-devel/RHOME/library/stats/html/glm.html}{glm})
 #'   \item validmu: logical function. Returns TRUE if a mean vector mu is within the domain of variance.
 #'   \item valideta: logical function. Returns TRUE if a linear predictor eta is within the domain of linkinv.
-#'         \href{https://stat.ethz.ch/R-manual/R-devel/RHOME/library/stats/html/simulate.html}{simulate}.
-#'         It will normally return a matrix with nsim columns and one row for each fitted value, but
-#'         it can also return a list of length nsim. Clearly this will be missing for ‘quasi-’ families.
+#'   \item simulate: (optional) function simulate(object, nsim) to be called by the "lm" method of
+#'         \href{https://stat.ethz.ch/R-manual/R-devel/library/stats/html/simulate.html}{simulate}.
+#'         It will normally return a matrix with nsim columns and one row for each fitted value,
+#'         but it can also return a list of length nsim. Clearly this will be missing for ‘quasi-’ families.
 #'         }
 #'
 #' @examples
@@ -45,6 +50,8 @@
 #' )
 #' @export
 binomialEF <- function(link = "loglog") {
+  # Code is a modification of stats's package family implementation.
+
   assertthat::assert_that(length(link) == 1, msg = "Argument link should have length 1.")
   assertthat::assert_that(is.character(link), msg = "Argument link should be a character.")
   assertthat::assert_that(link %in% c("loglog", "logc", "identity"), msg = "Argument link should be 'loglog', 'logc', or 'identity'.")
